d24ba32c0049fa0099ef5353279e1c47e972f1b5
Lalit <Chitibommal480@gmail.com>
2023-11-18 21:20:35 -0500

    Implemented AI

---
+++ compare.py
@@ -0,0 +1,15 @@
+from rake_nltk import Rake
+from sentence_transformers import SentenceTransformer, util
+
+def compare(text, definition):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    r = Rake()
+
+    r.extract_keywords_from_text(definition)
+    keywords = r.get_ranked_phrases()
+
+    en_1 = model.encode(text)
+    en_2 = model.encode(definition)
+    
+    cosine_scores = util.cos_sim(en_1, en_2)
+    return cosine_scores.item(), keywords
